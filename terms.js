0c80d8677a18614979d84058b3587ab60b9f8141
kotnid <tkt0506tkt@gmail.com>
2021-10-07 04:36:51 +0800

    split to 4 webs
    Signed-off-by: kotnid <tkt0506tkt@gmail.com>

---
+++ terms.js
@@ -0,0 +1,9 @@
+function check(){
+    var x = document.getElementById("mycheck").checked;
+    if (x== true){
+        // google form link put here 
+        location.replace("#")
+    }else{
+        alert ("Agree to the terms and condition first!")
+    }
+}
